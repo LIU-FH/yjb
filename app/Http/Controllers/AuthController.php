37c3b8a5f0614cd87825299e5559ae1815abb19f
LIU-FH <530005372@qq.com>
2020-04-16 19:37:53 +0800

    1

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,23 +8,35 @@ use App\Http\Resources\User\UserResource;
 use App\Models\User\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use EasyWeChat\Factory;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
         $params = $request->all();
+        $params['openId'] = '';
+        $params['unionId'] = '';
+        if(!empty($params['code'])){
+            $app = Factory::miniProgram(config('wechat'));
+            $res = $app->auth->session($params['code']);
+            Log::info("wechat",$res);
+            $params['openId'] = $res['openId'];
+            $params['unionId'] = $res['unionId'];
+        }
         if (!$obj = User::where('openid', $params['openId'])->first()) {
             $obj = new User();
+            $obj->parent_id = 0;
+            $obj->keyword = md5(md5(md5($params['openId'])));
         }
         $obj->openid = $params['openId'];
         $obj->unionid = $params['unionId'];
-        $obj->parent_id = $params['openId'];
-        $obj->nickname = $params['nickName'];
-        $obj->avatarurl = $params['avatarUrl'];
-        $obj->gender = $params['gender'];
-        $obj->last_login_time = $params['openId'];
-        if ($obj->save()) {
+        $obj->nickname = $params['userInfo']['nickName'];
+        $obj->avatarurl = $params['userInfo']['avatarUrl'];
+        $obj->gender = $params['userInfo']['gender'];
+        $obj->last_login_time = date("Y-m-d H:i:s",time());
+        if (!$obj->save()) {
             abort(5001);
         }
         return new UserResource($obj);
--- routes/api/auth.php
+++ routes/api/auth.php
@@ -1,7 +1,6 @@
 <?php
 Route::prefix('auth')->group(function () {
-    Route::post('logout', 'Auth\TokenController@logout')->name('token.logout');
-    Route::post('refresh', 'Auth\TokenController@refresh')->name('token.refresh');
+    Route::post('login', 'AuthController@login')->name('auth.login');
 });
 Route::post('upload', 'AuthController@upload')->name('auth.upload');
 
