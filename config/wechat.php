635b2c18d95e9b0602403125c37cec1dd363643f
LIU-FH <530005372@qq.com>
2020-04-24 03:57:25 +0800

    1

--- config/wechat.php
+++ config/wechat.php
@@ -10,12 +10,12 @@ return [
         'file' => storage_path('logs') . '/wechat.log',
     ],
 
-    'mch_id' => 'your-mch-id',
-    'key' => '11111111111111111111111111111111',   // API 密钥
+    'mch_id' => env("WX_MCH_ID"),
+    'key' => env("WX_KEY"),
 
     // 如需使用敏感接口（如退款、发送红包等）需要配置 API 证书路径(登录商户平台下载 API 证书)
     'cert_path' => 'path/to/your/cert.pem', // XXX: 绝对路径！！！！
     'key_path' => 'path/to/your/key',      // XXX: 绝对路径！！！！
 
-    'notify_url' => '默认的订单回调地址',     // 你也可以在下单时单独设置来想覆盖它
+    'notify_url' => 'https://yjb.liufh.com/api/v1/pay/wx/notify',     // 你也可以在下单时单独设置来想覆盖它
 ];
